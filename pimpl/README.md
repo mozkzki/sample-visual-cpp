ebdcd5734ecd41020a4f83a7deedde0da1532527
mozkzki <kato.yutaka@gmail.com>
2021-09-16 05:25:05 +0900

    pimpl追加

---
+++ pimpl/README.md
@@ -0,0 +1,3 @@
+# pimpl
+
+Pimplイディオム。
---
+++ pimpl/pimpl/inner_class.h
@@ -0,0 +1,12 @@
+﻿/* Copyright 2017 mozkzki */
+#pragma once
+
+class InnerClass {
+ public:
+    InnerClass() : number_(5) {}
+    ~InnerClass() {}
+    int GetNumber() { return this->number_; }
+
+ private:
+    int number_;
+};
